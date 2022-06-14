0a76fda7bc024d161372a1cad2f5144f430151e2
Quantum Snowball <quantum.snowball@gmail.com>
2022-06-14 16:43:57 +0800

    C-p can toggle cmp window open or close

--- lua/plugins/completion/nvimcmp/keymaps.lua
+++ lua/plugins/completion/nvimcmp/keymaps.lua
@@ -1,25 +1,37 @@
 return function (cmp, luasnip)
   return {
-    -- ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    -- scrolling
     ["<PageUp>"] = cmp.mapping.scroll_docs(-4),
-    -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<PageDown>"] = cmp.mapping.scroll_docs(4),
-    ["<C-P>"] = cmp.mapping.complete(),
+    -- toggle
+    ["<C-p>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.abort()
+      else cmp.complete()
+      end
+    end, { "i", "s" }
+    ),
+    -- confirm
     ["<CR>"] = cmp.mapping { i = cmp.mapping.confirm { select = true }, },
-    ["<Right>"] = cmp.mapping { i = cmp.mapping.confirm { select = true }, },
-    ["<C-e>"] = cmp.mapping.abort(),
+    -- navigate
     ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert }, { "i" }),
     ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert }, { "i" }),
+    -- navigate and jumping fields
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then cmp.select_next_item()
-      elseif luasnip.expandable() then luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
       else fallback()
       end
     end, { "i", "s", }),
     ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then luasnip.jump(-1)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
       else fallback()
       end
     end, { "i", "s", }),
