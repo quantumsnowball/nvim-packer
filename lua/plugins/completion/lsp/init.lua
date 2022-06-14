2d12e590d574cebc92f46299516aa74a7ae42477
Quantum Snowball <quantum.snowball@gmail.com>
2022-06-15 03:18:22 +0800

    ,fd to trigger auto format document using LSP

--- lua/plugins/completion/lsp/init.lua
+++ lua/plugins/completion/lsp/init.lua
@@ -1,5 +1,15 @@
 local use = require('packer').use
 
+-- LSP support for neovim
+use {
+  'neovim/nvim-lspconfig',
+  config = function()
+    -- format current document
+    vim.keymap.set('n', ',fd', vim.lsp.buf.formatting)
+    -- auto format document on save
+    -- vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
+  end
+}
 -- LSP installer
 -- https://github.com/williamboman/nvim-lsp-installer
 use {
@@ -8,7 +18,7 @@ use {
   config = function()
     local servers = require('plugins.completion.lsp.servers')
     -- ensure installed
-    require("nvim-lsp-installer").setup{
+    require("nvim-lsp-installer").setup {
       ensure_installed = servers,
       automatic_installation = true,
     }
