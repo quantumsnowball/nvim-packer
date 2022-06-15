be0742690972fed78c164737fd7652c9689c8a65
Quantum Snowball <quantum.snowball@gmail.com>
2022-06-16 03:15:35 +0800

    telescope remove ./ from fd results

--- lua/plugins/navigation/search/init.lua
+++ lua/plugins/navigation/search/init.lua
@@ -2,11 +2,16 @@ local use = require('packer').use
 
 use {
   'nvim-telescope/telescope.nvim',
-  requires = {'nvim-lua/plenary.nvim'},
+  requires = { 'nvim-lua/plenary.nvim' },
   config = function()
-    require('telescope').setup{
+    require('telescope').setup {
       defaults = {
         layout_config = { vertical = { width = 0.8 } }
+      },
+      pickers = {
+        find_files = {
+          find_command = { "fd", "--type", "f", "--strip-cwd-prefix" }
+        },
       }
     }
     -- telescope keymaps
