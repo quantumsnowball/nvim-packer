a260be3eea6f683c61aae62f47d3ae077615ee18
Quantum Snowball <quantum.snowball@gmail.com>
2022-06-16 18:25:49 +0800

    set theme only after theme has been loaded

--- lua/plugins/lookandfeel/theme/monokai.lua
+++ lua/plugins/lookandfeel/theme/monokai.lua
@@ -2,7 +2,9 @@ local use = require('packer').use
 
 -- colorful editor theme
 -- https://github.com/tanvirtin/monokai.nvim
-use 'tanvirtin/monokai.nvim'
-
--- set themes
-vim.cmd 'colorscheme monokai_ristretto'
+use {
+    'tanvirtin/monokai.nvim',
+    config = function()
+        vim.cmd 'colorscheme monokai_ristretto'
+    end
+}
