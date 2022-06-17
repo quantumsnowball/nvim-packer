1a99f5c7b5daa670ae39cfd8c6e0211d16158900
Quantum Snowball <quantum.snowball@gmail.com>
2022-06-17 17:34:30 +0800

    F3 to toggle theme variants for monokai

--- lua/plugins/lookandfeel/theme/monokai.lua
+++ lua/plugins/lookandfeel/theme/monokai.lua
@@ -5,7 +5,23 @@ local use = require('packer').use
 use {
     'tanvirtin/monokai.nvim',
     config = function()
+        -- enable
         vim.cmd 'colorscheme monokai_ristretto'
+        -- post theme
         require('plugins.lookandfeel.theme.after')
+        -- toggle theme variants
+        local current = 0
+        local variants = {
+            'monokai',
+            'monokai_pro',
+            'monokai_ristretto',
+            'monokai_soda',
+        }
+        vim.keymap.set('n', '<F3>', function()
+            local target = variants[math.fmod(current, 4) + 1]
+            vim.cmd('colorscheme ' .. target)
+            print(vim.inspect(target))
+            current = current + 1
+        end)
     end
 }
