aa4adf1379ee9997129e30bf871a254af21c48c1
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-01 02:40:37 +0800

    file outline panel plugin

--- lua/plugins/navigation/init.lua
+++ lua/plugins/navigation/init.lua
@@ -45,3 +45,22 @@ use {
         }
     end
 }
+-- outline
+use {
+    'simrat39/symbols-outline.nvim',
+    config = function()
+        vim.keymap.set('n', '<leader>o', ':SymbolsOutline<cr>')
+        vim.keymap.set('n', '<leader>m', ':SymbolsOutline<cr>')
+        -- override defaults
+        vim.g.symbols_outline = {
+            preview_bg_highlight = 'Comment',
+            winblend = 20,
+            auto_preview = false,
+            keymaps = {
+                focus_location = { "o", "H" },
+                hover_symbol = "K",
+                toggle_preview = { "J", "<Tab>" },
+            }
+        }
+    end
+}
