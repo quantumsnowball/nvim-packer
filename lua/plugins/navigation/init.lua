2f67497a4496d6892e0fa5f7119deb9a5ea5f270
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-02 01:22:38 +0800

    symbols-outline bg and url

--- lua/plugins/navigation/init.lua
+++ lua/plugins/navigation/init.lua
@@ -46,6 +46,7 @@ use {
     end
 }
 -- outline
+-- https://github.com/simrat39/symbols-outline.nvim
 use {
     'simrat39/symbols-outline.nvim',
     config = function()
@@ -53,8 +54,8 @@ use {
         vim.keymap.set('n', '<leader>m', ':SymbolsOutline<cr>')
         -- override defaults
         vim.g.symbols_outline = {
-            preview_bg_highlight = 'Comment',
-            winblend = 20,
+            preview_bg_highlight = 'Pmenu',
+            winblend = 30,
             auto_preview = false,
             keymaps = {
                 focus_location = { "o", "H" },
