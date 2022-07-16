2f67e7f1d7b50bf33ea562eb5d01d5f2204331e5
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-16 19:52:43 +0800

    comments

--- lua/plugins/navigation/session/init.lua
+++ lua/plugins/navigation/session/init.lua
@@ -7,11 +7,14 @@ use {
     config = function()
         -- plugin custom
         require('session_manager').setup({
-            autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
-            autosave_ignore_not_normal = false, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
+            -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+            autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
+            -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
+            autosave_ignore_not_normal = false,
         })
         -- autoload file explorer upon session loaded
-        local config_group = vim.api.nvim_create_augroup('SessionManagerCommandGroup', {}) -- A global group for all your config autocommands
+        local config_group = vim.api.nvim_create_augroup('SessionManagerCommandGroup', {})
+        -- A global group for all your config autocommands
         vim.api.nvim_create_autocmd({ 'SessionLoadPost' }, {
             group = config_group,
             callback = function()
