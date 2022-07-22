aa6a94718234050b57eaf33b4a33ec845b57b704
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-22 22:22:54 +0800

    refactor: use loop to set keymap and hints

--- lua/plugins/completion/lsp/lspsaga.lua
+++ lua/plugins/completion/lsp/lspsaga.lua
@@ -15,13 +15,24 @@ use({
         })
 
         -- keymaps
-        -- lsp finder to find the cursor word definition and reference
-        vim.keymap.set("n", "<leader>fs", require("lspsaga.finder").lsp_finder, { silent = true, noremap = true })
-        -- show hover doc, and scroll the doc
-        vim.keymap.set("n", "K", require("lspsaga.hover").render_hover_doc, { silent = true })
-        vim.keymap.set("n", "<C-d>", function() require("lspsaga.action").smart_scroll_with_saga(1) end,
-            { silent = true })
-        vim.keymap.set("n", "<C-u>", function() require("lspsaga.action").smart_scroll_with_saga(-1) end,
-            { silent = true })
+        local opts = { silent = true }
+        for _, item in pairs({
+            -- lsp finder to find the cursor word definition and reference
+            { "n", "<leader>fv", require("lspsaga.finder").lsp_finder, opts, 'lspsaga.lsp_finder()' },
+            -- show hover doc, and scroll the doc
+            { "n", "K", require("lspsaga.hover").render_hover_doc, opts, 'lspsaga.render_hover_doc()' },
+            { "n", "<C-d>", function() require("lspsaga.action").smart_scroll_with_saga(1) end, opts, },
+            { "n", "<C-u>", function() require("lspsaga.action").smart_scroll_with_saga(-1) end, opts, },
+        }) do
+            -- unpack values
+            -- -- neovim still using lua 5.1, new version use `table.unpack`
+            local mode, keys, command, options, hint = unpack(item)
+            -- register as vim key maps
+            vim.keymap.set(mode, keys, command, options)
+            -- register as which-key hints
+            if hint then
+                require('which-key').register({ [keys] = hint })
+            end
+        end
     end,
 })
