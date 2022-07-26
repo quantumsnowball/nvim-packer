6573455330753e45ad1f4fd7186554065e6d5673
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-27 02:13:41 +0800

    g stores telescope git, G for fugitive git

--- lua/plugins/editing/versioning/init.lua
+++ lua/plugins/editing/versioning/init.lua
@@ -25,4 +25,17 @@ use {
     end
 }
 -- git fugitive
-use 'tpope/vim-fugitive'
+use {
+    'tpope/vim-fugitive',
+    config = function()
+        for _, item in pairs({
+            { 'n', '<leader>Gd', ':Gvdiff<cr>', {}, 'telescope.diagnostics()' },
+        }) do
+            -- unpack values
+            -- -- neovim still using lua 5.1, new version use `table.unpack`
+            local mode, keys, command, options, hint = unpack(item)
+            vim.keymap.set(mode, keys, command, options)
+            require('which-key').register({ [keys] = hint })
+        end
+    end
+}
