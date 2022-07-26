bd6fec3e57991433b244e0986557f7f0ac4741d1
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-26 21:11:56 +0800

    arg table as var

--- lua/plugins/navigation/telescope/init.lua
+++ lua/plugins/navigation/telescope/init.lua
@@ -16,6 +16,7 @@ use {
         }
         -- telescope keymaps
         local tb = require('telescope.builtin')
+        local vlayout = { layout_strategy = "vertical" }
         for _, item in pairs({
             { 'n', '<leader>ff',
                 function()
@@ -28,46 +29,25 @@ use {
                         additional_args = function() return { "--hidden" } end })
                 end, {}, 'telescope.live_grep()' },
             -- find recent files
-            { 'n', '<leader>fr',
-                function()
-                    tb.oldfiles({ layout_strategy = "vertical" })
-                end, {}, 'telescope.oldfiles()' },
+            { 'n', '<leader>fr', function() tb.oldfiles(vlayout) end, {}, 'telescope.oldfiles()' },
             -- find buffers
-            { 'n', '<leader>fb',
-                function()
-                    tb.buffers({ layout_strategy = "vertical" })
-                end, {}, 'telescope.buffers()' },
+            { 'n', '<leader>fb', function() tb.buffers(vlayout) end, {}, 'telescope.buffers()' },
             -- find helps
-            { 'n', '<leader>fh',
-                function()
-                    tb.help_tags({ layout_strategy = "vertical" })
-                end, {}, 'telescope.help_tags()' },
+            { 'n', '<leader>fh', function() tb.help_tags(vlayout) end, {}, 'telescope.help_tags()' },
             -- find files, ignore hidden files
-            { 'n', '<leader>fnf',
-                function()
-                    tb.find_files({ layout_strategy = "vertical" })
-                end, {}, 'telescope.find_files(--no-hidden)' },
+            { 'n', '<leader>fnf', function() tb.find_files(vlayout) end, {}, 'telescope.find_files(--no-hidden)' },
             -- find words, ignore hidden files
-            { 'n', '<leader>fnw',
-                function()
-                    tb.live_grep({ layout_strategy = "vertical" })
-                end, {}, 'telescope.live_grep(--no-hidden)' },
+            { 'n', '<leader>fnw', function() tb.live_grep(vlayout) end, {}, 'telescope.live_grep(--no-hidden)' },
             -- grep visual selected text directly
-            { 'v', '<leader>fs',
-                function()
-                    tb.grep_string({ layout_strategy = "vertical" })
-                end, {}, 'telescope.grep_string(--visual-mode)' },
-            { 'v', '<leader>*',
-                function()
-                    tb.grep_string({ layout_strategy = "vertical" })
-                end, {}, 'telescope.grep_string(--visual-mode)' },
+            { 'v', '<leader>fs', function() tb.grep_string(vlayout) end, {}, 'telescope.grep_string(--visual-mode)' },
+            { 'v', '<leader>*', function() tb.grep_string(vlayout) end, {}, 'telescope.grep_string(--visual-mode)' },
         }) do
             -- unpack values
             -- -- neovim still using lua 5.1, new version use `table.unpack`
             -- local hint, mode, keys, command = unpack(item)
             local mode, keys, command, options, hint = unpack(item)
             -- register as vim key maps
-            vim.keymap.set(mode, keys, command)
+            vim.keymap.set(mode, keys, command, options)
             -- register as which-key hints
             require('which-key').register({ [keys] = hint })
         end
