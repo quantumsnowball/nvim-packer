52e3e6c6d0d4bdf31df93cb2a712be7c5ec1ba18
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-26 20:23:57 +0800

    telescope string command changed to lua functions

--- lua/plugins/navigation/telescope/init.lua
+++ lua/plugins/navigation/telescope/init.lua
@@ -15,45 +15,57 @@ use {
             }
         }
         -- telescope keymaps
+        local tb = require('telescope.builtin')
         for _, item in pairs({
             { 'n', '<leader>ff',
-                '<cmd>lua require("telescope.builtin").find_files({layout_strategy="vertical", hidden=true})<cr>', {},
-                'telescope.find_files()' },
+                function()
+                    tb.find_files({ layout_strategy = "vertical", hidden = true })
+                end, {}, 'telescope.find_files()' },
             -- find words, including hidden
             { 'n', '<leader>fg',
-                '<cmd>lua require("telescope.builtin").live_grep({layout_strategy="vertical", additional_args=function(opts) return {"--hidden"} end})<cr>',
-                {},
-                'telescope.live_grep()' },
+                function()
+                    tb.live_grep({ layout_strategy = "vertical",
+                        additional_args = function() return { "--hidden" } end })
+                end, {}, 'telescope.live_grep()' },
             { 'n', '<leader>fw',
-                '<cmd>lua require("telescope.builtin").live_grep({layout_strategy="vertical", additional_args=function(opts) return {"--hidden"} end})<cr>',
-                {}, 'telescope.live_grep()' },
+                function()
+                    tb.live_grep({ layout_strategy = "vertical",
+                        additional_args = function() return { "--hidden" } end })
+                end, {}, 'telescope.live_grep()' },
             -- find recent files
             { 'n', '<leader>fr',
-                '<cmd>lua require("telescope.builtin").oldfiles({layout_strategy="vertical"})<cr>', {},
-                'telescope.oldfiles()' },
+                function()
+                    tb.oldfiles({ layout_strategy = "vertical" })
+                end, {}, 'telescope.oldfiles()' },
             -- find buffers
             { 'n', '<leader>fb',
-                '<cmd>lua require("telescope.builtin").buffers({layout_strategy="vertical"})<cr>', {},
-                'telescope.buffers()' },
+                function()
+                    tb.buffers({ layout_strategy = "vertical" })
+                end, {}, 'telescope.buffers()' },
             -- find helps
             { 'n', '<leader>fh',
-                '<cmd>lua require("telescope.builtin").help_tags({layout_strategy="vertical"})<cr>', {},
-                'telescope.help_tags()' },
+                function()
+                    tb.help_tags({ layout_strategy = "vertical" })
+                end, {}, 'telescope.help_tags()' },
             -- find files, ignore hidden files
             { 'n', '<leader>fnf',
-                '<cmd>lua require("telescope.builtin").find_files({layout_strategy="vertical"})<cr>', {},
-                'telescope.find_files(--no-hidden)' },
+                function()
+                    tb.find_files({ layout_strategy = "vertical" })
+                end, {}, 'telescope.find_files(--no-hidden)' },
             -- find words, ignore hidden files
             { 'n', '<leader>fng',
-                '<cmd>lua require("telescope.builtin").live_grep({layout_strategy="vertical"})<cr>', {},
-                'telescope.live_grep(--no-hidden)' },
+                function()
+                    tb.live_grep({ layout_strategy = "vertical" })
+                end, {}, 'telescope.live_grep(--no-hidden)' },
             -- grep visual selected text directly
             { 'v', '<leader>fs',
-                '<cmd>lua require("telescope.builtin").grep_string({layout_strategy="vertical"})<cr>', {},
-                'telescope.grep_string(--visual-mode)' },
+                function()
+                    tb.grep_string({ layout_strategy = "vertical" })
+                end, {}, 'telescope.grep_string(--visual-mode)' },
             { 'v', '<leader>*',
-                '<cmd>lua require("telescope.builtin").grep_string({layout_strategy="vertical"})<cr>', {},
-                'telescope.grep_string(--visual-mode)' },
+                function()
+                    tb.grep_string({ layout_strategy = "vertical" })
+                end, {}, 'telescope.grep_string(--visual-mode)' },
         }) do
             -- unpack values
             -- -- neovim still using lua 5.1, new version use `table.unpack`
