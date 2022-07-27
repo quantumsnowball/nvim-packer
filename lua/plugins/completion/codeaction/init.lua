d70c107bc7f855da6e4187e579b9a13d9ff8f758
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-27 18:44:01 +0800

    remove duplicated lightbulb plugin

--- lua/plugins/completion/codeaction/init.lua
+++ lua/plugins/completion/codeaction/init.lua
@@ -9,13 +9,3 @@ use {
 }
 -- this plugin is lazy loading so preconfig the keymap before it is first loaded
 vim.keymap.set('n', '<leader>qf', ':CodeActionMenu<cr>')
-
--- code action lightbulb gutter icon
--- https://github.com/kosayoda/nvim-lightbulb
-use {
-    'kosayoda/nvim-lightbulb',
-    requires = 'antoinemadec/FixCursorHold.nvim',
-    config = function()
-        require('nvim-lightbulb').setup({ autocmd = { enabled = true } })
-    end
-}
