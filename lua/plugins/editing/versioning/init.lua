6fd275559bbf33d360fbebcc8e8d6391d5afac59
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-29 05:52:32 +0800

    make lazygit full window

--- lua/plugins/editing/versioning/init.lua
+++ lua/plugins/editing/versioning/init.lua
@@ -50,6 +50,9 @@ use {
 use {
     'kdheepak/lazygit.nvim',
     config = function()
+        -- config
+        vim.g.lazygit_floating_window_scaling_factor = 1.0
+        vim.g.lazygit_floating_window_use_plenary = 1
         -- toggle keymap
         vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>')
         vim.keymap.set('n', '<leader>gl', ':LazyGit<CR>')
