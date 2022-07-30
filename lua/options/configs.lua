d871deadf79a8e99b1bee40f380416d87a92bd97
Quantum Snowball <quantum.snowball@gmail.com>
2022-07-30 20:11:27 +0800

    virtualedit in all mode

--- lua/options/configs.lua
+++ lua/options/configs.lua
@@ -19,3 +19,5 @@ vim.cmd('command! WipeReg for i in range(34,122) | silent! call setreg(nr2char(i
 vim.cmd('set iskeyword+=-')
 -- write and delete buffer command
 vim.cmd [[cnoreabbrev wd w\|bd"]]
+-- virtualedit in all modes
+vim.opt.virtualedit = 'all'
