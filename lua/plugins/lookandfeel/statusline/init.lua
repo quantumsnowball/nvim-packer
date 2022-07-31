0fbb0c4c5a713ed98d083325b5afd2f7e39d6a51
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-01 02:14:54 +0800

    move incline to statusline module

--- lua/plugins/lookandfeel/statusline/init.lua
+++ lua/plugins/lookandfeel/statusline/init.lua
@@ -1,2 +1,5 @@
+-- status line
 require('plugins.lookandfeel.statusline.lualine')
 -- require('plugins.lookandfeel.statusline.airline')
+-- winbar-like
+require('plugins.lookandfeel.statusline.incline')
--- lua/plugins/navigation/init.lua
+++ lua/plugins/navigation/init.lua
@@ -64,40 +64,4 @@ use {
             }
         }
     end
-};
--- show buffer path at top right corner
--- https://github.com/b0o/incline.nvim
-use {
-    'b0o/incline.nvim',
-    config = function()
-        -- define its own highligh group
-        vim.cmd [[ autocmd VimEnter,ColorScheme * hi InclineActive guifg=#deff00 guibg=#555555 gui=bold ]]
-        vim.cmd [[ autocmd VimEnter,ColorScheme * hi InclineInactive guifg=#deff00 guibg=#444444 ]]
-        -- config
-        require('incline').setup({
-            hide = {
-                cursorline = 'focused_win'
-            },
-            highlight = {
-                groups = {
-                    InclineNormal = {
-                        default = true,
-                        group = "InclineActive"
-                    },
-                    InclineNormalNC = {
-                        default = true,
-                        group = "InclineInactive"
-                    }
-                }
-            },
-            -- output formatting function
-            render = function(props)
-                -- reference :help filename-modifiers
-                local path = vim.api.nvim_buf_get_name(props.buf)
-                local cwd = vim.fn.getcwd()
-                -- full path relative to cwd
-                return string.gsub(path, cwd .. '/', '')
-            end
-        })
-    end
 }
