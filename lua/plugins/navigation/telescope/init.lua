7505892db30aa9aacc707cae7085f1cdfb0a67c8
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-11 05:36:47 +0800

    fzf search native for telescope

--- lua/plugins/navigation/telescope/init.lua
+++ lua/plugins/navigation/telescope/init.lua
@@ -6,6 +6,7 @@ use {
     'nvim-telescope/telescope.nvim',
     requires = {
         'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope-fzf-native.nvim',
         'nvim-telescope/telescope-ui-select.nvim'
     },
     config = function()
@@ -57,6 +58,13 @@ use {
                 highlights = { layout_config = { width = 0.7 } },
             },
             extensions = {
+                fzf = {
+                    fuzzy = true, -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true, -- override the file sorter
+                    case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                },
                 ["ui-select"] = {
                     require("telescope.themes").get_dropdown({
                         border = true
@@ -64,6 +72,7 @@ use {
                 }
             },
         }
+        require('telescope').load_extension('fzf')
         require("telescope").load_extension("ui-select")
         -- telescope keymaps
         local keymaps = require('plugins.navigation.telescope.keymaps').keymaps
@@ -72,6 +81,13 @@ use {
             require('utils').map(mode, keys, command, options, hint)
         end
     end
+}
+
+-- fzf telescope extension
+-- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+use {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    run = 'make'
 }
 
 -- better quickfix list
