7e109c24c3ea4b845c003130a82fe4bf9320bca0
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-14 14:13:35 +0800

    rf: use map function

--- lua/options/keymaps.lua
+++ lua/options/keymaps.lua
@@ -1,108 +1,106 @@
 -- global leader key
 vim.g.mapleader = ','
+
 -- copy and paste to system clipboard
 vim.cmd 'set clipboard^=unnamed,unnamedplus'
+
 -- keymaps and hints
-for _, item in pairs({
-    -------------
-    -- general --
-    -------------
-    -- quit windo shortcut
-    { 'n', 'qw', ':q<cr>' },
-    { 'n', 'qW', ':qa<cr>' },
-    -- show which-key root
-    { 'n', '<F1>', ':WhichKey<cr>', { silent = true } },
-    -- toggle split windows
-    { 'n', 'g-', ':split<cr>' },
-    { 'n', 'g\\', ':vsplit<cr>' },
-    -- toggle a new tabpage
-    { 'n', '<F12>', ':tabnew<cr>' },
-    -- cancel search highlight after search
-    { 'n', 'znn', ':noh<cr>', {}, 'Clear search result highlight' },
-    -- G also center line
-    { 'n', 'G', 'Gzz' },
-    -- <leader>Q records macro, q is free
-    { 'n', '<leader>q', 'q' },
-    { 'n', 'q', '<Nop>' },
-    -- toggle spell checking
-    { 'n', '<leader>sc', ':set spell!<cr>' },
-    -- quick set shiftwidth
-    { 'n', '<leader>2', ':set shiftwidth=2<cr>' },
-    { 'n', '<leader>4', ':set shiftwidth=4<cr>' },
-    -- home and end in normal mode
-    { { 'n', 'v', 'o' }, 'gh', '^', {}, 'Home' },
-    { { 'n', 'v', 'o' }, 'gl', '$', {}, 'End' },
-    -- home and end in insert mode
-    { 'i', '<C-h>', '<home>' },
-    { 'i', '<C-l>', '<end>' },
-    -- shift-tab will go one tab backward
-    { 'i', '<S-Tab>', '<C-d>' },
-    -- | and \ will break line before and after cursor
-    { 'n', '|', 'a<C-m><esc>' },
-    { 'n', '\\', 'i<C-m><esc>' },
-    -- go and gO will open new line but stay in normal mode
-    { 'n', 'gO', 'O<esc>' },
-    { 'n', 'go', 'o<esc>' },
-    -- C-o will also open new line in insert mode
-    { 'i', '<C-o>', '<esc>o' },
-    -- utils
-    { 'n', '<F7>', require('options.utils').refresh_configs, {}, 'utils.refresh_configs()' },
-    { 'n', '<leader>rl', require('options.utils').refresh_configs, {}, 'utils.refresh_configs()' },
-    -- toggle word wrap
-    { 'n', 'gw', ':set wrap!<cr>' },
-    -- use Tab to toggle matching group
-    -- -- normal mode: matching brackets or tags (by matchit.vim, need to set remap=true)
-    -- -- visual mode: easily select the opposite matching whole section
-    -- -- operation pending mode: can delete, yank, or cut the whole section, very handy!
-    { { 'n', 'v', 'o' }, '<Tab>', '%', { remap = true } },
-    { { 'n', 'v', 'o' }, '[<Tab>', '[m', { remap = true } },
-    { { 'n', 'v', 'o' }, ']<Tab>', ']m', { remap = true } },
-    ---------------
-    -- clipboard --
-    ---------------
-    -- swap ; :
-    { 'n', ';', ':' },
-    -- delete
-    { 'n', 'x', '"_x' },
-    { 'v', 'x', '"_x' },
-    { 'n', 'X', '"_X' },
-    { 'v', 'X', '"_X' },
-    { 'n', 'd', '"_d' },
-    { 'v', 'd', '"_d' },
-    { 'n', 'D', '"_D' },
-    { 'v', 'D', '"_D' },
-    -- replace
-    { 'n', 'c', '"_c' },
-    { 'v', 'c', '"_c' },
-    { 'n', 'C', '"_C' },
-    { 'v', 'C', '"_C' },
-    -- cut
-    { 'n', '<leader>x', '"+x' },
-    { 'v', '<leader>x', '"+x' },
-    { 'n', '<leader>X', '"+X' },
-    { 'v', '<leader>X', '"+X' },
-    { 'n', '<leader>d', '"+d' },
-    { 'v', '<leader>d', '"+d' },
-    { 'n', '<leader>D', '"+D' },
-    { 'v', '<leader>D', '"+D' },
-    { 'n', '<leader>c', '"+c' },
-    { 'v', '<leader>c', '"+c' },
-    { 'n', '<leader>C', '"+C' },
-    { 'v', '<leader>C', '"+C' },
-    -- visual mode replace without copying the deleted text
-    { 'v', 'p', '"_dP' },
-    { 'v', 'P', '"_dP' },
-    -- cursor position after paste
-    { 'n', 'P', 'gP' },
-    { 'n', 'gP', 'P' },
-    { 'n', 'p', 'p`]' },
-    -- *aa as change all text operation
-    { 'n', 'vaa', 'ggVG', {}, 'select all text' },
-    { 'n', 'yaa', 'ggVGy', {}, 'copy all text' },
-    { 'n', 'daa', 'ggVG"_d', {}, 'delete all text' },
-    { 'n', '<leader>daa', 'ggVGd', {}, 'cut all text' },
-    { 'n', 'caa', 'ggVG"_c', {}, 'change all text' },
-}) do
-    local mode, keys, command, options, hint = unpack(item)
-    require('utils').map(mode, keys, command, options, hint)
-end
+local map = require('utils').map
+-------------
+-- general --
+-------------
+-- quit windo shortcut
+map('n', 'qw', ':q<cr>')
+map('n', 'qW', ':qa<cr>')
+-- show which-key root
+map('n', '<F1>', ':WhichKey<cr>', { silent = true })
+-- toggle split windows
+map('n', 'g-', ':split<cr>')
+map('n', 'g\\', ':vsplit<cr>')
+-- toggle a new tabpage
+map('n', '<F12>', ':tabnew<cr>')
+-- cancel search highlight after search
+map('n', 'znn', ':noh<cr>', {}, 'Clear search result highlight')
+-- G also center line
+map('n', 'G', 'Gzz')
+-- <leader>Q records macro, q is free
+map('n', '<leader>q', 'q')
+map('n', 'q', '<Nop>')
+-- toggle spell checking
+map('n', '<leader>sc', ':set spell!<cr>')
+-- quick set shiftwidth
+map('n', '<leader>2', ':set shiftwidth=2<cr>')
+map('n', '<leader>4', ':set shiftwidth=4<cr>')
+-- home and end in normal mode
+map({ 'n', 'v', 'o' }, 'gh', '^', {}, 'Home')
+map({ 'n', 'v', 'o' }, 'gl', '$', {}, 'End')
+-- home and end in insert mode
+map('i', '<C-h>', '<home>')
+map('i', '<C-l>', '<end>')
+-- shift-tab will go one tab backward
+map('i', '<S-Tab>', '<C-d>')
+-- | and \ will break line before and after cursor
+map('n', '|', 'a<C-m><esc>')
+map('n', '\\', 'i<C-m><esc>')
+-- go and gO will open new line but stay in normal mode
+map('n', 'gO', 'O<esc>')
+map('n', 'go', 'o<esc>')
+-- C-o will also open new line in insert mode
+map('i', '<C-o>', '<esc>o')
+-- utils
+map('n', '<F7>', require('options.utils').refresh_configs, {}, 'utils.refresh_configs()')
+map('n', '<leader>rl', require('options.utils').refresh_configs, {}, 'utils.refresh_configs()')
+-- toggle word wrap
+map('n', 'gw', ':set wrap!<cr>')
+-- use Tab to toggle matching group
+-- -- normal mode: matching brackets or tags (by matchit.vim, need to set remap=true)
+-- -- visual mode: easily select the opposite matching whole section
+-- -- operation pending mode: can delete, yank, or cut the whole section, very handy!
+map({ 'n', 'v', 'o' }, '<Tab>', '%', { remap = true })
+map({ 'n', 'v', 'o' }, '[<Tab>', '[m', { remap = true })
+map({ 'n', 'v', 'o' }, ']<Tab>', ']m', { remap = true })
+---------------
+-- clipboard --
+---------------
+-- swap ; :
+map('n', ';', ':')
+-- delete
+map('n', 'x', '"_x')
+map('v', 'x', '"_x')
+map('n', 'X', '"_X')
+map('v', 'X', '"_X')
+map('n', 'd', '"_d')
+map('v', 'd', '"_d')
+map('n', 'D', '"_D')
+map('v', 'D', '"_D')
+-- replace
+map('n', 'c', '"_c')
+map('v', 'c', '"_c')
+map('n', 'C', '"_C')
+map('v', 'C', '"_C')
+-- cut
+map('n', '<leader>x', '"+x')
+map('v', '<leader>x', '"+x')
+map('n', '<leader>X', '"+X')
+map('v', '<leader>X', '"+X')
+map('n', '<leader>d', '"+d')
+map('v', '<leader>d', '"+d')
+map('n', '<leader>D', '"+D')
+map('v', '<leader>D', '"+D')
+map('n', '<leader>c', '"+c')
+map('v', '<leader>c', '"+c')
+map('n', '<leader>C', '"+C')
+map('v', '<leader>C', '"+C')
+-- visual mode replace without copying the deleted text
+map('v', 'p', '"_dP')
+map('v', 'P', '"_dP')
+-- cursor position after paste
+map('n', 'P', 'gP')
+map('n', 'gP', 'P')
+map('n', 'p', 'p`]')
+-- *aa as change all text operation
+map('n', 'vaa', 'ggVG', {}, 'select all text')
+map('n', 'yaa', 'ggVGy', {}, 'copy all text')
+map('n', 'daa', 'ggVG"_d', {}, 'delete all text')
+map('n', '<leader>daa', 'ggVGd', {}, 'cut all text')
+map('n', 'caa', 'ggVG"_c', {}, 'change all text')
