1d30d0b0b36d33f4b1bcd9a4bed092b59611b7f6
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-14 13:37:15 +0800

    rf: telescope configs table

--- lua/plugins/navigation/telescope/init.lua
+++ lua/plugins/navigation/telescope/init.lua
@@ -10,76 +10,9 @@ use {
         'nvim-telescope/telescope-ui-select.nvim'
     },
     config = function()
-        require('telescope').setup {
-            defaults = {
-                layout_strategy = 'vertical',
-                layout_config = {
-                    vertical = {
-                        width = 0.85,
-                        preview_height = 0.65,
-                        preview_cutoff = 20
-                    }
-                },
-                mappings = {
-                    n = {
-                        o = 'toggle_all',
-                        p = require('telescope.actions.layout').toggle_preview,
-                        J = require('telescope.actions').cycle_history_next,
-                        K = require('telescope.actions').cycle_history_prev,
-                    },
-                    i = {
-                        ["<C-j>"] = require('telescope.actions').cycle_history_next,
-                        ["<C-k>"] = require('telescope.actions').cycle_history_prev,
-                        ["<Down>"] = require('telescope.actions').cycle_history_next,
-                        ["<Up>"] = require('telescope.actions').cycle_history_prev,
-                    },
-                },
-                sorting_strategy = 'ascending',
-                scroll_strategy = 'limit',
-                initial_mode = 'normal',
-                border = true,
-                dynamic_preview_title = true,
-                prompt_prefix = '🔍 ',
-                cache_picker = { num_pickers = 50 }
-            },
-            pickers = {
-                find_files = { initial_mode = 'insert', },
-                live_grep = { initial_mode = 'insert' },
-                jumplist = { show_line = false },
-                help_tags = { initial_mode = 'insert' },
-                buffers = {
-                    layout_config = { width = 0.7, height = 0.8, preview_height = 0.5, },
-                    mappings = { n = { x = "delete_buffer" } },
-                },
-                current_buffer_fuzzy_find = { initial_mode = 'insert' },
-                registers = { initial_mode = 'insert' },
-                builtin = { layout_config = { preview_height = 0.4 } },
-                colorscheme = { layout_config = { width = 0.2, height = 0.7 } },
-                spell_suggest = { layout_config = { width = 0.2, height = 0.7 } },
-                command_history = { layout_config = { width = 0.6 } },
-                highlights = { layout_config = { width = 0.7 } },
-                lsp_definitions = {
-                    jump_type = 'never',
-                    fname_width = 60,
-                    trim_text = true,
-                    layout_config = { preview_height = 0.73 }
-                }
-            },
-            extensions = {
-                fzf = {
-                    fuzzy = true, -- false will only do exact matching
-                    override_generic_sorter = true, -- override the generic sorter
-                    override_file_sorter = true, -- override the file sorter
-                    case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-                    -- the default case_mode is "smart_case"
-                },
-                ["ui-select"] = {
-                    require("telescope.themes").get_cursor({
-                        border = true
-                    }),
-                }
-            },
-        }
+        -- configs
+        local configs = require('plugins.navigation.telescope.configs')
+        require('telescope').setup(configs)
         -- load extensions
         require('telescope').load_extension('fzf')
         require("telescope").load_extension("ui-select")
