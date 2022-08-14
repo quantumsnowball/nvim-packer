ff362e4405eb2c1286e73d0f53dede37160c4e88
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-14 17:16:48 +0800

    rf: map function for diagnostics

--- lua/plugins/editing/diagnostics/init.lua
+++ lua/plugins/editing/diagnostics/init.lua
@@ -14,23 +14,21 @@ use {
             auto_close = true
         }
         -- keymaps
-        local options = { silent = true, noremap = true }
-        for _, item in pairs({
-            { "n", "<leader>ss", "<cmd>TroubleToggle<cr>", },
-            { "n", "<leaderw", "<cmd>Trouble workspace_diagnostics<cr>", },
-            { "n", "<leader>sd", "<cmd>Trouble document_diagnostics<cr>", },
-            { "n", "<leader>sl", "<cmd>Trouble loclist<cr>", },
-            { "n", "<leader>sq", "<cmd>Trouble quickfix<cr>", },
-            { "n", "gR", "<cmd>Trouble lsp_references<cr>", },
-            { "n", "<leader>sn", function()
+        local map = require('utils').map
+        local opts = { silent = true, noremap = true }
+        map("n", "<leader>ss", "<cmd>TroubleToggle<cr>", opts)
+        map("n", "<leaderw", "<cmd>Trouble workspace_diagnostics<cr>", opts)
+        map("n", "<leader>sd", "<cmd>Trouble document_diagnostics<cr>", opts)
+        map("n", "<leader>sl", "<cmd>Trouble loclist<cr>", opts)
+        map("n", "<leader>sq", "<cmd>Trouble quickfix<cr>", opts)
+        map("n", "gR", "<cmd>Trouble lsp_references<cr>", opts)
+        map("n", "<leader>sn",
+            function()
                 require("trouble").next({ skip_groups = true, jump = true })
-            end, "Trouble next()" },
-            { "n", "<leader>sN", function()
+            end, opts, "Trouble next()")
+        map("n", "<leader>sN",
+            function()
                 require("trouble").previous({ skip_groups = true, jump = true })
-            end, "Trouble previous()" },
-        }) do
-            local mode, keys, command, hint = unpack(item)
-            require('utils').map(mode, keys, command, options, hint)
-        end
+            end, opts, "Trouble previous()")
     end
 }
