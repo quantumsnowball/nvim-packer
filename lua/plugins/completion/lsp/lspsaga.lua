44f156e027cbbfc15efdd05d4312809322a3160e
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-14 16:49:24 +0800

    rf: map function for lspsaga

--- lua/plugins/completion/lsp/lspsaga.lua
+++ lua/plugins/completion/lsp/lspsaga.lua
@@ -22,32 +22,30 @@ use({
         })
 
         -- keymaps
+        local map = require('utils').map
         local opts = { silent = true }
-        for _, item in pairs({
-            -- show hover doc, and scroll the doc
-            { "n", "K", require("lspsaga.hover").render_hover_doc, opts, 'lspsaga.render_hover_doc()' },
-            { "n", "<C-n>", function() require("lspsaga.action").smart_scroll_with_saga(1) end, opts, },
-            { "n", "<C-p>", function() require("lspsaga.action").smart_scroll_with_saga(-1) end, opts, },
-            -- show signature help
-            { "n", "zk", require("lspsaga.signaturehelp").signature_help, opts, 'lspsaga.signaturehelp()' },
-            { "i", "<C-k>", require("lspsaga.signaturehelp").signature_help, opts, 'lspsaga.signaturehelp()' },
-            -- rename, close rename win use <C-c> in insert mode or `q` in normal mode or `:q`
-            { "n", "<leader>rn", require("lspsaga.rename").lsp_rename, opts, 'lspsaga.rename()' },
-            -- diagnostic and errors
-            { "n", "<space>e", require("lspsaga.diagnostic").show_line_diagnostics, opts, 'lspsaga.diagnostic()' },
-            { "n", "[e", require("lspsaga.diagnostic").goto_prev, opts, 'lspsaga.diagnostic.goto_prev()' },
-            { "n", "]e", require("lspsaga.diagnostic").goto_next, opts, 'lspsaga.diagnostic.goto_next()' },
-            { "n", "[E", function()
+        -- show hover doc, and scroll the doc
+        map("n", "K", require("lspsaga.hover").render_hover_doc, opts, 'lspsaga.render_hover_doc()')
+        map("n", "<C-n>", function() require("lspsaga.action").smart_scroll_with_saga(1) end, opts)
+        map("n", "<C-p>", function() require("lspsaga.action").smart_scroll_with_saga(-1) end, opts)
+        -- show signature help
+        map("n", "zk", require("lspsaga.signaturehelp").signature_help, opts, 'lspsaga.signaturehelp()')
+        map("i", "<C-k>", require("lspsaga.signaturehelp").signature_help, opts, 'lspsaga.signaturehelp()')
+        -- rename, close rename win use <C-c> in insert mode or `q` in normal mode or `:q`
+        map("n", "<leader>rn", require("lspsaga.rename").lsp_rename, opts, 'lspsaga.rename()')
+        -- diagnostic and errors
+        map("n", "<space>e", require("lspsaga.diagnostic").show_line_diagnostics, opts, 'lspsaga.diagnostic()')
+        map("n", "[e", require("lspsaga.diagnostic").goto_prev, opts, 'lspsaga.diagnostic.goto_prev()')
+        map("n", "]e", require("lspsaga.diagnostic").goto_next, opts, 'lspsaga.diagnostic.goto_next()')
+        map("n", "[E",
+            function()
                 require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
-            end, opts, 'lspsaga.diagnostic.goto_prev(ERROR)' },
-            { "n", "]E", function()
+            end, opts, 'lspsaga.diagnostic.goto_prev(ERROR)')
+        map("n", "]E",
+            function()
                 require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
-            end, opts, 'lspsaga.diagnostic.goto_next(ERROR)' },
-            -- float terminal
-            { 'n', '<leader>tt', require('lspsaga.floaterm').open_float_terminal, opts, 'lspsaga.open_float_terminal()' }
-        }) do
-            local mode, keys, command, options, hint = unpack(item)
-            require('utils').map(mode, keys, command, options, hint)
-        end
+            end, opts, 'lspsaga.diagnostic.goto_next(ERROR)')
+        -- float terminal
+        map('n', '<leader>tt', require('lspsaga.floaterm').open_float_terminal, opts, 'lspsaga.open_float_terminal()')
     end,
 })
