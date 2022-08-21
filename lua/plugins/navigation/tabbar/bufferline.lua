fbc4941dc6719bd23b6283eee50acb4cb37bbb3d
Quantum Snowball <quantum.snowball@gmail.com>
2022-08-22 04:48:15 +0800

    bufferline diagnostics icons and counts

--- lua/plugins/navigation/tabbar/bufferline.lua
+++ lua/plugins/navigation/tabbar/bufferline.lua
@@ -23,6 +23,17 @@ use {
                         end
                     end
                     return false
+                end,
+                -- diagnostics infos
+                diagnostics = 'nvim_lsp',
+                diagnostics_indicator = function(_, _, diagnostics_dict, _)
+                    local s = " "
+                    for e, n in pairs(diagnostics_dict) do
+                        local sym = e == "error" and " "
+                            or (e == "warning" and " " or "")
+                        s = s .. n .. sym
+                    end
+                    return s
                 end
             }
         }
