5d53baae8e402a7f7e95c143b65eafddcabf50cb
Quantum Snowball <quantum.snowball@gmail.com>
2022-12-03 13:01:00 +0800

    normal gj and gk jumps between empty lines

--- lua/plugins/navigation/jumping/init.lua
+++ lua/plugins/navigation/jumping/init.lua
@@ -17,22 +17,26 @@ use {
 
 -- jumplist related utils function
 -- navigation through jumplist
-local isJumpingForward = true
-local function toggleJumps()
-    if (isJumpingForward) then
-        vim.cmd('call feedkeys("gj")')
-    else
-        vim.cmd('call feedkeys("gk")')
-    end
-    isJumpingForward = not isJumpingForward
-end
+-- local isJumpingForward = true
+-- local function toggleJumps()
+--     if (isJumpingForward) then
+--         vim.cmd('call feedkeys("gj")')
+--     else
+--         vim.cmd('call feedkeys("gk")')
+--     end
+--     isJumpingForward = not isJumpingForward
+-- end
 
 -- keymaps
 local map = require('utils').map
 -- back and forward on jumplist
-map('n', 'gj', '<C-o>', {}, 'jumpBackward()')
-map('n', 'gk', '<C-i>', {}, 'jumpForward()')
-map('n', 'g;', toggleJumps, {}, 'toggleJumps()')
+-- map('n', 'gj', '<C-o>', {}, 'jumpBackward()')
+-- map('n', 'gk', '<C-i>', {}, 'jumpForward()')
+-- map('n', 'g;', toggleJumps, {}, 'toggleJumps()')
 
 -- autocmd
 vim.cmd('autocmd VimEnter * clearjumps')
+
+-- gj, gk simply jumping between sections
+map('n', 'gj', '}')
+map('n', 'gk', '{')
