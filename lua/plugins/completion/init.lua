73946f7c9b9b0fcb85af31213585ed724a3be29f
Quantum Snowball <quantum.snowball@gmail.com>
2022-12-09 16:47:34 +0800

    lite version don't install snippets

--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -3,6 +3,6 @@ require('plugins.completion.lsp')
 -- cmp
 require('plugins.completion.nvimcmp')
 -- snippets
-require('plugins.completion.snippets')
+-- require('plugins.completion.snippets')
 -- codeaction
 require('plugins.completion.codeaction')
