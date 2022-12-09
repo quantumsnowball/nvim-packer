c954cfd40a827a8368f90a6c255cf7376065b4cc
Quantum Snowball <quantum.snowball@gmail.com>
2022-12-09 16:27:29 +0800

    lite version don't ensure install any lsp server

--- lua/plugins/completion/lsp/installer/servers.lua
+++ lua/plugins/completion/lsp/installer/servers.lua
@@ -1,12 +1,12 @@
 return {
-    'pyright', -- python
-    'pylsp', -- python
-    'sumneko_lua', -- lua
-    'tsserver', -- js, ts
-    'cssls', -- css
-    'html', -- html
-    'jsonls', -- json, json5
-    'solc', -- solidity
-    'clangd', -- cpp
-    'zk'
+    -- 'pyright', -- python
+    -- 'pylsp', -- python
+    -- 'sumneko_lua', -- lua
+    -- 'tsserver', -- js, ts
+    -- 'cssls', -- css
+    -- 'html', -- html
+    -- 'jsonls', -- json, json5
+    -- 'solc', -- solidity
+    -- 'clangd', -- cpp
+    -- 'zk'
 }
