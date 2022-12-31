e411905952d4f38388e8ef968835eef25f03c0fc
Quantum Snowball <quantum.snowball@gmail.com>
2022-12-31 13:21:00 +0800

    nvim-tree highlight the current file in explorer

--- lua/plugins/navigation/explorer/nvimtree.lua
+++ lua/plugins/navigation/explorer/nvimtree.lua
@@ -13,6 +13,9 @@ use {
                 },
             },
             reload_on_bufenter = true,
+            update_focused_file = {
+                enable = true,
+            },
             view = {
                 float = {
                     enable = true,
