b93cd9bcc516c8515a0a6688eef47d6d9d8be36a
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-29 20:07:10 +0800

    sonokai F3 switching variant helper

--- lua/plugins/lookandfeel/theme/sonokai.lua
+++ lua/plugins/lookandfeel/theme/sonokai.lua
@@ -8,8 +8,27 @@ use {
         -- enable the theme and choose the variant
         -- options:
         --   default, atlantis, andromeda, shusia, maia, espresso
-        vim.g.sonokai_style = 'atlantis'
+        vim.g.sonokai_style = 'andromeda'
         -- vim.g.sonokai_better_performance = 1
         vim.cmd('colorscheme sonokai')
+        -- toggle theme variants helper
+        local current = 3
+        local variants = {
+            'default',
+            'atlantis',
+            'andromeda',
+            'shusia',
+            'maia',
+            'espresso'
+        }
+        local map = require('utils').map
+        map('n', '<F3>', function()
+            local target = variants[math.fmod(current, 4) + 1]
+            vim.g.sonokai_style = target
+            vim.cmd('colorscheme sonokai')
+            print(vim.inspect(target))
+            current = current + 1
+        end)
+        require('which-key').register({ ['<F3>'] = 'toggle_colorscheme()' })
     end
 }
