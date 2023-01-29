3f837152cdba59c1d127f25be32cd20c26fe563b
Quantum Snowball <quantum.snowball@gmail.com>
2023-01-29 19:58:07 +0800

    switch from monokai to sonokai
    P.S. monokai changed some color, some how don't render TS parser correctly
    sonokai.nvim is a better choice, matching best with OneHalf Dark Pallette of Windows Terminal

--- lua/plugins/lookandfeel/theme/init.lua
+++ lua/plugins/lookandfeel/theme/init.lua
@@ -1,8 +1,10 @@
---[[
+--
 -- switch themes here
---]]
-require('plugins.lookandfeel.theme.monokai')
--- require('plugins.lookandfeel.theme.sonokai')
+--
+-- require('plugins.lookandfeel.theme.monokai')
+-- P.S. monokai changed some color, some how don't render TS parser correctly
+-- sonokai.nvim is a better choice, matching best with OneHalf Dark Pallette of Windows Terminal
+require('plugins.lookandfeel.theme.sonokai')
 
 -- enable transparent for all theme
 require('plugins.lookandfeel.theme.nvim-transparent')
